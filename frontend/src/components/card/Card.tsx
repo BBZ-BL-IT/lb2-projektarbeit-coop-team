a200b670b0a41e6c72d3487f6a259b0377d1cbf9
topptobi <tobias.topp@gmx.ch>
2025-09-22 11:05:09 +0200

    Added Switch animation for Cards

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,13 +1,18 @@
+import {useState} from "react";
 import "./Card.css";
 
 export default function Card() {
-    const relaxo = '/pokemon-image/Relaxo.png'
-
+    const relaxo = "/pokemon-image/Relaxo.png";
+    const [isFlipped, setIsFlipped] = useState(false);
 
     return (
-
-            <div className="card-container">
-                <img src={relaxo}/>
+        <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={() => setIsFlipped((!isFlipped))}>
+            <div className="card-inner">
+                <div className="card-face card-front"></div>
+                <div className="card-face card-back">
+                    <img src={relaxo} alt="Relaxo"/>
+                </div>
             </div>
+        </div>
     );
 }
