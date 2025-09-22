325391d2e432d723b81a770ac3b66c7f7bef7227
topptobi <tobias.topp@gmx.ch>
2025-09-22 08:32:11 +0200

    Added new Card Component

--- frontend/src/pages/game-page/GamePage.tsx
+++ frontend/src/pages/game-page/GamePage.tsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
+import Card from "../../components/card/Card";
 
 export default function GamePage() {
-  const { id } = useParams();
-  return (
-    <div>
-      <h1>Game-Page</h1>
-      <p>Current Game: {id}</p>
-    </div>
-  );
-}
+    const {id} = useParams();
+    return (
+        <div>
+            <h1>Game-Page</h1>
+
+            <div>
+                <Card></Card>
+            </div>
+        </div>
+    );
+};
