267646b211986369e850b8de0e8b4e51f5b008e8
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-10-15 00:39:59 +0200

    Refactor layout styles in App.css and Footer.css for improved structure and consistency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,24 @@ import Navbar from "./components/navbar/Navbar";
 import { AuthProvider } from "./contexts/AuthContext";
 import GamePage from "./pages/game-page/GamePage";
 import HomePage from "./pages/home-page/HomePage";
+import MultiplayerGamePage from "./pages/multiplayer-game-page/MultiplayerGamePage";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/game/:id" element={<GamePage />} />
-          <Route path="*" element={<h1>404: Page Not Found</h1>} />
-        </Routes>
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/game/:id" element={<GamePage />} />
+            <Route
+              path="/multiplayer/:gameId"
+              element={<MultiplayerGamePage />}
+            />
+            <Route path="*" element={<h1>404: Page Not Found</h1>} />
+          </Routes>
+        </main>
         <Footer />
       </BrowserRouter>
     </AuthProvider>
