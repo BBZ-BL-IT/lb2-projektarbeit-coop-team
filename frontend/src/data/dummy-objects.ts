8693cb6e8ba80957177d3c173a5fc1aa2e169bf2
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-10-15 18:53:36 +0200

    Update userStatsMock

--- frontend/src/data/dummy-objects.ts
+++ frontend/src/data/dummy-objects.ts
@@ -1,9 +1,21 @@
 // Dummy Object Example
 export const userStatsMock = {
-  userName: "Kiwii",
+  email: "lorenz@mail.com",
   wins: 10,
-  losses: 5,
-  draws: 2,
-  totalGames: 17,
-  winRate: 58.82,
+  losses: 7,
+  winRate: 58.82, // Calculated as (wins / totalGames) * 100
+  totalTimePlayed: 3600,
+  totalGamesPlayed: 17, // Calculated as wins + losses
+  averageGameDuration: 210, // Calculated as totalTimePlayed / totalGames
+  highestScore: 5,
+  totalMatchedPairs: 8,
 };
+
+// In the database:
+// email
+// wins
+// losses
+// totalTimePlayed
+// totalGamesPlayed
+// highestScore
+// totalMatchedPairs
